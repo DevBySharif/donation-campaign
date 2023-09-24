6627560d4c557844961d3c6356a42aaa599a6525
Sharif Obaidullah <sharifmdobaidullah@gmail.com>
2023-09-24 23:50:55 +0600

    Added banner with serach bar

--- src/Components/Header/Banner/SearchBar/SearchBar.jsx
+++ src/Components/Header/Banner/SearchBar/SearchBar.jsx
@@ -1,8 +1,13 @@
 const SearchBar = () => {
   return (
     <div>
-      <input className="w-[360px] h-10" type="text" />
-      <button className="bg-[#FF444A] px-6 py-3">Search</button>
+      <input
+        className="w-[360px] py-[8px] border-solid border-2 border-gray-200 rounded-l-lg"
+        type="text"
+      />
+      <button className="bg-[#FF444A] px-6 py-[9px] rounded-r-lg text-base text-white font-semibold">
+        Search
+      </button>
     </div>
   );
 };
